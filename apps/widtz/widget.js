4af4f731180d41070f74e69b33fae39198048048
stephenPspackman <93166870+stephenPspackman@users.noreply.github.com>
2023-12-15 18:27:25 -0800

    Update widget.js
    Fix borken rendering.

--- apps/widtz/widget.js
+++ apps/widtz/widget.js
@@ -6,17 +6,17 @@
     width:24,
     height:24,
     draw:function() {
-      var settings = require('Storage').readJSON('setting.json',1)||{};
+      const settings = require('Storage').readJSON('setting.json',1)||{};
       var vp = this.y+11;
       g.reset();
-      g.drawRect(this.x, this.y, this.x+this.width-1, this.y+this.height-1);
-      g.setColor(g.getBgColor);
-      g.fillRect(this.x+1, this.y+1, this.x+this.width-2, this.y+this.height-2);
-      g.reset();
+      g.setColor(settings.bg);
+      g.fillRect(this.x, this.y, this.x+this.width-1, this.y+this.height-1);
+      g.setColor(settings.fg);
       g.setFontAlign(0,-1);
-      g.setFont('6x8');
-      g.drawString(settings.timezoneLabel || 'GMT', this.x+this.width/2, this.y+2);
-      if (settings.timezoneLabel) {
+      const id = settings.timezoneid || 'GMT';
+      g.setFont(id.length <= 3 ? '6x8' : '4x5');
+      g.drawString(id, this.x+this.width/2, this.y+2);
+      if (settings.timezoneid) {
         vp = this.y+14;
         g.drawLine(this.x+3, this.y+12, this.x+this.width-4, this.y+12);
       }
@@ -27,6 +27,8 @@
       if (tzm) {
         tzf += ':' + ('' + tzm).padStart(2, '0');
         g.setFont('4x5');
+      } else {
+        g.setFont('6x8');
       }
       g.drawString(tzf, this.x+this.width/2, vp);
     }
